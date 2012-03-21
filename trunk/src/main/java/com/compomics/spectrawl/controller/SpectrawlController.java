6fd1d920f2e131e1e36a547e72fb4a4400d0dc30
nielshulstaert <nielshulstaert@447f5218-c8c3-37d9-3585-788f3fd345ba>
2012-03-21 16:23:19 +0000

    added gui modifications

--- trunk/src/main/java/com/compomics/spectrawl/controller/SpectrawlController.java
+++ trunk/src/main/java/com/compomics/spectrawl/controller/SpectrawlController.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.compomics.spectrawl.gui;
+package com.compomics.spectrawl.controller;
 
 /**
  *
