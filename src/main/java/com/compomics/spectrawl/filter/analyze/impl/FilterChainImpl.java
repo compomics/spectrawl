25bd5beefa93c0b02bc26efcf536e694f7a4e0d5
mvaudel <mvaudel@447f5218-c8c3-37d9-3585-788f3fd345ba>
2012-04-27 09:06:38 +0000

    extended the constructor for a chain with or without modification

--- src/main/java/com/compomics/spectrawl/filter/analyze/impl/FilterChainImpl.java
+++ src/main/java/com/compomics/spectrawl/filter/analyze/impl/FilterChainImpl.java
@@ -9,6 +9,7 @@ import com.compomics.util.experiment.biology.ions.ReporterIon;
 
 import java.util.ArrayList;
 import java.util.List;
+import sun.misc.Perf;
 
 /**
  * Created by IntelliJ IDEA. User: niels Date: 16/02/12 Time: 14:02 To change
@@ -75,8 +76,10 @@ public class FilterChainImpl<T> implements FilterChain<T> {
 
     /**
      * Creates a filter based on a peptide fragment
-     *
-     * @param peptideFragment the peptide fragment of interest as a String
+     * @param peptideFragment the peptide fragment
+     * @param accountedCharges the accounted charges
+     * @param accountedPtms the accounted PTMs
+     * @return the corresponding filter chain
      */
     public static FilterChainImpl getFilterChain(String peptideFragment, ArrayList<Integer> accountedCharges, ArrayList<PTM> accountedPtms) {
         FilterChainImpl result = new FilterChainImpl(FilterChainType.OR);
@@ -98,5 +101,28 @@ public class FilterChainImpl<T> implements FilterChain<T> {
             result.addFilter(combFilter);
         }
         return result;
+    }
+
+    /**
+     * Creates a filter based on a peptide fragment
+     * @param peptideFragment the peptide fragment
+     * @param accountedCharges the accounted charges
+     * @return the corresponding filter chain
+     */
+    public static FilterChainImpl getFilterChain(String peptideFragment, ArrayList<Integer> accountedCharges) {
+        return getFilterChain(peptideFragment, accountedCharges, new ArrayList<PTM>());
+    }
+    
+    /**
+     * Creates a filter based on a peptide fragment
+     * @param peptideFragment the peptide fragment
+     * @param accountedCharges the accounted charges
+     * @param ptm the accounted PTM
+     * @return the corresponding filter chain
+     */
+    public static FilterChainImpl getFilterChain(String peptideFragment, ArrayList<Integer> accountedCharges, PTM ptm) {
+        ArrayList<PTM> ptms = new ArrayList<PTM>();
+        ptms.add(ptm);
+        return getFilterChain(peptideFragment, accountedCharges, ptms);
     }
 }
